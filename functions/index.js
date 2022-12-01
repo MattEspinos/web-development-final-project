ab50a1451dd10035583559ef2af83cbbb85d6a89
mattespinos <matthewwespinos@lewisu.edu>
2022-11-30 22:55:52 -0600

    Updated landing page and everything else

---
+++ functions/index.js
@@ -0,0 +1,40 @@
+const functions = require("firebase-functions");
+const bodyParser = require("body-parser");
+const express = require("express");
+const { engine } = require("express-handlebars");
+const path = require("path");
+const cors = require("cors")({origin: true});
+
+//setting up cors and bodyparser
+const app = express();
+app.use(cors);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+// view engine setup
+app.engine('hbs', engine());
+app.set("views", path.join(__dirname, "./public/pages"));
+app.set("view engine", "hbs");
+
+//routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/home", (req, res) => {
+    res.render("index");
+});
+
+app.get("/test", function(req, res) {
+    res.render("test");
+});
+
+
+process.on("uncaughtException", function(err) {
+  console.log(err);
+});
+exports.app = functions.https.onRequest(app);
+/* eslint-disable eol-last */
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
-    entry: './index.js',
+    entry: './firebase.js',
     output: {
         path: path.resolve(__dirname, 'public/js'),
         filename: 'bundle.js'
@@ -11,7 +11,7 @@ module.exports = {
         rules: [
             {
                 test: /\.s?[ac]ss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                use: ['style-loader', 'css-loade+r', 'sass-loader'],
                 exclude: [/node_modules/],
               },
         ],
